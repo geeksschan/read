421af8dedfe06e90e4f751d5ae62052d3888f443
sechan.seo <sechan.seo@nhn.com>
2019-03-28 16:09:53 +0900

    [마크업] 헤더 UI 변경

---
+++ src/components/common/Aside.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import style from './Aside.scss';
+
+class Aside extends Component {
+  render() {
+    return (
+      <div className={}>
+
+      </div>
+    );
+  }
+}
+
+export default Aside;
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -20,15 +20,10 @@ class Header extends Component {
           <a href="/" className={style.link_title}>
             <h1 className={style.title}>읽어라 읽어</h1>
           </a>
-          <div className={style.feature_group}>
-            <a href="/" className={style.link_search}>검색</a>
-            <a href="/" className={style.link_new_post}><span className={style.icon_new_post}>+</span> 새 글쓰기</a>
-            { !isLogin ? (
-              <a href="/" className={style.link_login}>로그인</a>
-            ) : (
-              <DropdownUser username={username} thumbnailUrl={thumbnailUrl} />
-            )}
-          </div>
+          <button type="button" className={style.button_menu} aria-haspopup="true" aria-expanded="false">
+            <span className={style.icon_menu}></span>
+            <span className="blind">메뉴</span>
+          </button>
         </div>
       </header>
     );
